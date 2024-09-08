bf3572c06f7a4900e0a63b7c7df41452e4c7d7e4
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-07 23:59:32 -0700

    zsh config sets neovim as default editor if possible

--- oh-my-zsh/startup.zsh
+++ oh-my-zsh/startup.zsh
@@ -1,5 +1,12 @@
 # figlet -cf lean "Welcome"
 fastfetch
 
+# Set neovim as default editor if possible
+if command -v nvim >/dev/null 2>&1; then
+    export EDITOR="nvim"
+else
+    export EDITOR="vim"
+fi
+
 alias fucking="sudo"
 alias nvimconf="cd ~/.config/nvim/ && $EDITOR"
